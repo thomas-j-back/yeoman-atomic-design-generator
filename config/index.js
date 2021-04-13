ee3794be5bee32eb921960c55fbae5d97742dbac
Thomas Back <thomasback@IntelleiveTBMBP.attlocal.net>
2021-04-12 23:08:16 -0700

    adding methods create and add for generator

---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports = ATOMIC_OBJECTS = [
+  "atom",
+  "molecule",
+  "organism",
+  "template",
+  "page",
+];
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,11 +1,36 @@
 const Generator = require("yeoman-generator");
+const config = require("../../config");
 
 module.exports = class extends Generator {
-  method1() {
-    this.log("method 1 just ran");
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.command = args[0];
+    this.atomic_entity = args[1];
+    this.entity_name = args[2];
   }
 
-  method2() {
-    this.log("method 2 just ran");
+  /**
+   * Handle generator command accordingly
+   */
+  handleCommand() {
+    if (this.command && typeof this[this.command] === "function") {
+      this[this.command]();
+    } else {
+      console.error(
+        "Invalid argument at index 0. Valid options are create and add"
+      );
+      return;
+    }
   }
+  /**
+   * Will initialize the folder structure
+   * if none already exist under src/components
+   */
+  create() {}
+
+  /**
+   * Add an atomic element
+   */
+  add() {}
 };
