dd9e189b185a77bfbfaf8e779845a8d01ca7a047
Thomas Back <thomasback@IntelleiveTBMBP.attlocal.net>
2021-04-14 20:06:45 -0700

    more testing

---
+++ generators/templates/element/_class-component.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+class <%= name %> extends React.Component {
+
+    render() {
+        return <h1>Start coding...</h1>;
+    }
+}
---
+++ generators/templates/element/_functional-component.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const <%= name %> = (props) => {
+    return <p>I am a component</p>;
+}
